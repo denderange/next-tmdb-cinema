ecf65b644a7bd6369791143e3d5926589d48e532
dennispolukaroff <dennispolukaroff@gmail.com>
2024-06-14 17:22:59 +0300

    fc

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum aliquam laboriosam nihil fuga. Quam iusto qui magni ipsum quia repudiandae soluta rem dicta nesciunt sunt voluptatum itaque inventore, iure excepturi!</main>
+  );
+}
