ccd82ebca83f3d565cbebe58b19e62b649a1a039
Dennis Polukaroff <dennispolukaroff@gmail.com>
2024-06-15 17:15:28 +0300

    add navbar

---
+++ src/app/about/page.tsx
@@ -0,0 +1,34 @@
+const About = () => {
+	return (
+		<div className='max-w-6xl mx-auto p-3 space-y-4 indent-6'>
+			<h1 className='text-2xl font-medium text-amber-600 indent-0'>About</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi magnam
+				deserunt soluta impedit modi totam ex reiciendis. In vero laboriosam
+				ullam impedit qui perspiciatis magni eius, dolor odit dolorum facilis?
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi magnam
+				deserunt soluta impedit modi totam ex reiciendis. In vero laboriosam
+				ullam impedit qui perspiciatis magni eius, dolor odit dolorum facilis?
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi magnam
+				deserunt soluta impedit modi totam ex reiciendis. In vero laboriosam
+				ullam impedit qui perspiciatis magni eius, dolor odit dolorum facilis?
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi magnam
+				deserunt soluta impedit modi totam ex reiciendis. In vero laboriosam
+				ullam impedit qui perspiciatis magni eius, dolor odit dolorum
+				facilis?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+				magnam deserunt soluta impedit modi totam ex reiciendis. In vero
+				laboriosam ullam impedit qui perspiciatis magni eius, dolor odit dolorum
+				facilis?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+				magnam deserunt soluta impedit modi totam ex reiciendis. In vero
+				laboriosam ullam impedit qui perspiciatis magni eius, dolor odit dolorum
+				facilis?
+			</p>
+		</div>
+	);
+};
+
+export default About;
